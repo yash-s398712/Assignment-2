5f69e8ce273e4d79985b5a3a784783c6c41c01a8
Bismark2005 <bismarkmensah439@gmail.com>
2026-01-14 21:26:51 +0930

    Implemented CSV file loading logic

--- Q2/temperature_analysis.py
+++ Q2/temperature_analysis.py
@@ -2,3 +2,13 @@ import os
 import pandas as pd
 
 FOLDER_PATH = "temperatures"
+
+all_data = []
+
+for file in os.listdir(FOLDER_PATH):
+    if file.endswith(".csv"):
+        year = file.split("_")[-1].replace(".csv", "")
+        file_path = os.path.join(FOLDER_PATH, file)
+
+        df = pd.read_csv(file_path)
+        all_data.append(df)
